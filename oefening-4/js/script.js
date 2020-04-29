0080d6583f78600db566c9b4c52baed1e53691ee
saraclaeys <sara.claeys01@gmail.com>
2020-04-29 12:55:12 +0200

    style letter counter

--- oefening-4/js/script.js
+++ oefening-4/js/script.js
@@ -1,3 +1,4 @@
-function countLet(obj) {
-    document.getElementById('letterNum').innerHTML = 'Lengte tekst: ' + obj.value.length;
+function lengte() {
+    let zin = document.querySelector(".text").value;
+    document.querySelector('.uitkomst').innerHTML = "de zin is " + zin.length + " tekens lang";
 }
